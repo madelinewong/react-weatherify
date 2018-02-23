d5fd2cf5fc8072b5727b61743dc71f10e79521f8
Madeline Wong <madelinegwong@outlook.com>
2018-02-23 15:36:59 -0500

    made minor changes, such as fixed my mispelling of lat and also added minimal styling to the app.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state= {
+    this.state = {
       lat: 0,
-      lon: 0, 
+      lon: 0,
       currentWeather: {},
       error: null
     };
-this.handleLatChange = this.handleLatChange.bind(this);
-this.handleLonChange = this.handleLonChange.bind(this);
-this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleLatChange = this.handleLatChange.bind(this);
+    this.handleLonChange = this.handleLonChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleLatChange(e) {
@@ -27,7 +27,7 @@ this.handleSubmit = this.handleSubmit.bind(this);
     });
   }
   handleSubmit(e) {
-    e.preventDefault(); 
+    e.preventDefault();
     console.log("submitting some things");
   }
   render() {
@@ -36,28 +36,29 @@ this.handleSubmit = this.handleSubmit.bind(this);
         <h1>Weatherify!</h1>
         <p>lat/lon</p>
         <form onSubmit={e => this.handleSubmit(e)}>
-        <label>
-          lat:
-          <input 
-          type="number"
-          min="-90"
-          max="90"
-          onChange={e => this.handleLatChange(e)}
-          value={this.state.lat}
-          required
-          />
-        </label>
-        <label>
-          lon: 
-          <input 
-          type="number"
-          min="-180"
-          max="180"
-          onChange={e => this.handleLonChange(e)}
-          value={this.state.lon}
-          required/>
-        </label>
-        <button type="submit">get weather now</button>
+          <label>
+            lat:
+            <input
+              type="number"
+              min="-90"
+              max="90"
+              onChange={e => this.handleLatChange(e)}
+              value={this.state.lat}
+              required
+            />
+          </label>
+          <label>
+            lon:
+            <input
+              type="number"
+              min="-180"
+              max="180"
+              onChange={e => this.handleLonChange(e)}
+              value={this.state.lon}
+              required
+            />
+          </label>
+          <button type="submit">get weather now</button>
         </form>
       </div>
     );
