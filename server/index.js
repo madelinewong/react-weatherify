cc1d62b2b07a87dc7213c790f1d491dd97e24a90
Madeline Wong <madelinegwong@outlook.com>
2018-02-23 19:08:12 -0500

    added new files.

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const axios = require("axios");
+
+require('dotenv').config();
+const { API_KEY } = process.env;
+
+const serverApp = express();
+const port = process.env.PORT || 5000;
+
+serverApp.get("/forecast/:lat,:lon", function(request, response) {
+  const { lat, lon } = request.params;
+  const url =
+    `https://api.darksky.net/forecast/${API_KEY}/${lat},${lon}`;
+  axios
+    .get(url)
+    .then(res => {
+      response.status(200).json(res.data);
+    })
+    .catch(err => {
+      response.status(500).json({
+        msg: "your stuff is broke."
+      });
+    });
+});
+
+serverApp.listen(port, () => {
+  console.log(`now listening on port ${port}`);
+});
